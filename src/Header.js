413bbc1156c75cdf29aec4882494d009b385df3b
RyuSueYeon <gmail8586@naver.com>
2025-11-13 14:37:05 +0900

    헤더, 롤팀 파일 생성

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+
+import blogTitle from './logo.svg';
+import './Header.css';
+import React, { useState } from 'react';
+
+function Header() {
+
+  
+  return (
+    <div className="Header">
+
+
+    </div>
+  );
+}
+
+
+export default Header;
---
+++ src/LoLTeam.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+
+import blogTitle from './logo.svg';
+import './LoLTeam.css';
+import React, { useState } from 'react';
+
+function LoLTeam() {
+
+  
+  return (
+    <div className="LoLTeam">
+
+
+    </div>
+  );
+}
+
+
+export default LoLTeam;
